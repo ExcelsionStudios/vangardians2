afda5c13f15edd41f1c5afaeba5ea40fa4514c09
Esai Solorio <sanziosan@live.com>
2015-05-20 14:45:48 -0500

    Added a scene with path prediction, I made two versions of fling projectile so there is also an extra script called "FlingProjectileWithPredict"

--- Assets/Project/Scripts/FlingProjectileWithPathPredict.cs
+++ Assets/Project/Scripts/FlingProjectileWithPathPredict.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FlingProjectile : MonoBehaviour {
+public class FlingProjectileWithPathPredict : MonoBehaviour {
 	public GameObject projectilePrefab;
 	public float upwardForce;
 	public float sideForce;
@@ -10,13 +10,23 @@ public class FlingProjectile : MonoBehaviour {
 	public float maxForce;
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
 			lastMousePosition = Input.mousePosition;
+		}
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			direction = new Vector2 (Mathf.Clamp(Input.mousePosition.x - lastMousePosition.x, -maxForce, maxForce)/maxForce,Mathf.Clamp(Input.mousePosition.y - lastMousePosition.y, -maxForce, maxForce)/maxForce );
+			GameObject newProjectile = (GameObject)Instantiate(projectilePrefab, gameObject.transform.position, projectilePrefab.transform.rotation);
+			newProjectile.GetComponent<Rigidbody>().AddForce(Vector3.up * upwardForce);
+			newProjectile.GetComponent<Rigidbody>().AddForce(new Vector3(-direction.x, 0, -direction.y) * sideForce);
+			Color projectileColor = newProjectile.GetComponent<Renderer>().material.color;
+			projectileColor.a = .5f;
+			newProjectile.GetComponent<Renderer>().material.color = projectileColor;
+			Destroy(newProjectile.GetComponent<Collider>());
 		}
 		if (Input.GetKeyUp(KeyCode.Mouse0)) {
 			direction = new Vector2 (Mathf.Clamp(Input.mousePosition.x - lastMousePosition.x, -maxForce, maxForce)/maxForce,Mathf.Clamp(Input.mousePosition.y - lastMousePosition.y, -maxForce, maxForce)/maxForce );
@@ -25,6 +35,6 @@ public class FlingProjectile : MonoBehaviour {
 			newProjectile.GetComponent<Rigidbody>().AddForce(new Vector3(-direction.x, 0, -direction.y) * sideForce);
 		}
 	}
-	
-	
+
+
 }
