a027ad573f1ac91980384739293a93439502d5d2
Epitome87 <rubywep@hotmail.com>
2015-05-20 13:48:42 -0700

    - Quickly tweaked the prediction to use its own separate prefab for the "predictor" (which is for now just a ball, admittedly we'll want probably a dotted lien or straight line, but we're doing this very quickly for a meeting that should've started an hour ago) - Made an interval timer, so you don't constantly see a bunch of huge ballz.

--- Assets/Project/Scripts/FlingProjectile.cs
+++ Assets/Project/Scripts/FlingProjectile.cs
@@ -11,6 +11,12 @@ public class FlingProjectile : MonoBehaviour {
 	public bool usePrediction;
 
 	public Material transparentMaterial;
+
+
+	public GameObject predictionPrefab;
+	public float predictionInterval = 0.5f;
+	private float timeUntilPrediction = 0.5f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -18,18 +24,29 @@ public class FlingProjectile : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+		if (Input.GetKeyDown(KeyCode.Mouse0)) 
+		{
 			lastMousePosition = Input.mousePosition;
 		}
-		if (usePrediction && Input.GetKey (KeyCode.Mouse0)) {
-			direction = new Vector2 (Mathf.Clamp(Input.mousePosition.x - lastMousePosition.x, -maxForce, maxForce)/maxForce,Mathf.Clamp(Input.mousePosition.y - lastMousePosition.y, -maxForce, maxForce)/maxForce );
-			GameObject newProjectile = (GameObject)Instantiate(projectilePrefab, gameObject.transform.position, projectilePrefab.transform.rotation);
-			newProjectile.GetComponent<Rigidbody>().AddForce(Vector3.up * upwardForce);
-			newProjectile.GetComponent<Rigidbody>().AddForce(new Vector3(-direction.x, 0, -direction.y) * sideForce);
-			newProjectile.GetComponent<Renderer>().material = transparentMaterial;
-			Destroy(newProjectile.GetComponent<Collider>());
+
+		if (usePrediction && Input.GetKey(KeyCode.Mouse0)) 
+		{
+			timeUntilPrediction -= Time.deltaTime;
+			if (timeUntilPrediction <= 0f)
+			{
+				timeUntilPrediction = predictionInterval;
+
+				direction = new Vector2 (Mathf.Clamp(Input.mousePosition.x - lastMousePosition.x, -maxForce, maxForce)/maxForce,Mathf.Clamp(Input.mousePosition.y - lastMousePosition.y, -maxForce, maxForce)/maxForce );
+				GameObject newProjectile = (GameObject)Instantiate(predictionPrefab, gameObject.transform.position, predictionPrefab.transform.rotation);
+				newProjectile.GetComponent<Rigidbody>().AddForce(Vector3.up * upwardForce);
+				newProjectile.GetComponent<Rigidbody>().AddForce(new Vector3(-direction.x, 0, -direction.y) * sideForce);
+				newProjectile.GetComponent<Renderer>().material = transparentMaterial;
+				Destroy(newProjectile.GetComponent<Collider>());
+			}
 		}
-		if (Input.GetKeyUp(KeyCode.Mouse0)) {
+
+		if (Input.GetKeyUp(KeyCode.Mouse0)) 
+		{
 			direction = new Vector2 (Mathf.Clamp(Input.mousePosition.x - lastMousePosition.x, -maxForce, maxForce)/maxForce,Mathf.Clamp(Input.mousePosition.y - lastMousePosition.y, -maxForce, maxForce)/maxForce );
 			GameObject newProjectile = (GameObject)Instantiate(projectilePrefab, gameObject.transform.position, projectilePrefab.transform.rotation);
 			newProjectile.GetComponent<Rigidbody>().AddForce(Vector3.up * upwardForce);
