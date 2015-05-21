7b9f652e6ffdbfa85d039f4f4f400c742e27dd6f
Esai Solorio <sanziosan@live.com>
2015-05-21 13:00:59 -0500

    Added my own scene folder cause i felt left out :<, made an exploding projectile(the explosion not only kills on impact but moves other enemies around it as well)

---
+++ Assets/ExplodeOnImpact.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeOnImpact : MonoBehaviour {
+
+	public GameObject explodePrefab;
+	// Use this for initialization
+	void OnCollisionEnter(Collision col){
+		Instantiate (explodePrefab, gameObject.transform.position, explodePrefab.transform.rotation);
+	}
+
+}
