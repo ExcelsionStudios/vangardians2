4bbc967dcf75b54089dce17e68b7660b8f8ec009
Esai Solorio <sanziosan@live.com>
2015-05-21 16:56:00 -0500

    Made a fancy scrollable menu for the projectiles we currently have

---
+++ Assets/ChangeProjectiles.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeProjectiles : MonoBehaviour {
+
+	public FlingProjectilePrediction playerTower;
+	public void changePrefab(GameObject obj){
+		playerTower.projectilePrefab = obj;
+	}
+}
