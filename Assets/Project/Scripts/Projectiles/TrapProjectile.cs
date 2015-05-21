ffcf0177b7ee18fb1b4a2eb641eeffc18f0bd833
Epitome87 <rubywep@hotmail.com>
2015-05-21 11:51:19 -0700

    - Bear Trap. But it disappears for some reason :( Halp! - Gave Land Mine the scripts for adding to Player Score and Kills.

---
+++ Assets/Project/Scripts/Projectiles/TrapProjectile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Matt McGrath - 5/21/2015
+public class TrapProjectile : MonoBehaviour 
+{
+	void OnCollisionEnter(Collision col)
+	{
+		// If we collide with an  Enemy, "trap" that Enemy.
+		if(col.gameObject.tag == "Enemy")
+		{
+			Enemy enemy = col.gameObject.GetComponent<Enemy>();
+			MoveTo moveToScript = enemy.GetComponent<MoveTo>();
+			moveToScript.speed = 0f;
+		}
+	}
+}
