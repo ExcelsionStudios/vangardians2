e7cfc5c101a97459b0ac710ee5a39c6ed1ed51d3
Esai Solorio <sanziosan@live.com>
2015-05-21 16:12:15 -0500

    ScatterShots!

---
+++ Assets/Project/Scripts/ScatterShot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScatterShot : MonoBehaviour {
+
+	void Update () {
+		if (gameObject.GetComponent<Rigidbody> ().velocity.y < 0) {
+			
+			
+			
+			
+			foreach(Transform spawnPoint in gameObject.transform.Find("SpawnPoints").GetComponentsInChildren<Transform>()){
+				GameObject newProjectile = (GameObject)Instantiate(gameObject, spawnPoint.position, gameObject.transform.rotation);
+				newProjectile.transform.localScale *= .5f;
+				Destroy (newProjectile.GetComponent<ScatterShot>());
+				newProjectile.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity;
+			}
+			
+			
+			
+			Destroy (gameObject);
+		}
+	}
+}
