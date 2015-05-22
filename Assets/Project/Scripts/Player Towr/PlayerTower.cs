7a6e91fc78c59a957cb61585413c1b42397807f1
Epitome87 <rubywep@hotmail.com>
2015-05-22 13:20:44 -0700

    - Quickly got the Tower ready to have its health reduced and shown in a UI. So Esai can do the logic for enemies hitting the tower and reducing its health.

--- Assets/Project/Scripts/Player Towr/PlayerTower.cs
+++ Assets/Project/Scripts/Player Towr/PlayerTower.cs
@@ -1,33 +1,39 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
+// Matt McGrath 5/21/2015
+
+// The PlayerTower is the building the player is both controlling and protecting.
+// Controlling this tower currently takes place through the ProjectileSpawnerPrediction class.
 public class PlayerTower : MonoBehaviour 
 {
-
-	// Use this for initialization
-	void Start () 
-	{
+	public float MaxHealth;
 	
+	public float Health
+	{
+		get { return health; }
+		set { health = Mathf.Clamp(value, 0f, MaxHealth); }
+	}
+	private float health;
+
+	// Quick-and-dirty Text to show the Tower's Health. Set this in inspector. TODO Health Meter?
+	public Text healthText;
+
+	// Use this for initialization: Simply sets Health to Max for now.
+	void Start()
+	{
+		Health =  MaxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		// Update the health text's text value to show current health.
+		healthText.text = Health + " / " + MaxHealth;
 	}
 
-//	void OnCollisionEnter(Collision col)
-//	{
-//		// If we collide with an  Enemy, Destroy that Enemy.
-//		if (col.gameObject.tag == "Enemy")
-//		{
-//			Debug.Log ("Enemy Entered Tower!");
-//			Enemy enemy = col.gameObject.GetComponent<Enemy>();
-//			EnemyPointCounter.Score -= enemy.KillPoints / 4;
-//			Destroy(col.gameObject);
-//		}
-//	}
-
+	// ** ALTER THIS! ** We don't want enemies just disappearing anymore. We want them to linger and attack the Player Tower.
 	void OnTriggerEnter(Collider other)
 	{
 		// If we collide with an  Enemy, Destroy that Enemy.
