44e9a095af3515de9d96f97c8f228f12ee436163
Esai Solorio <sanziosan@live.com>
2015-05-22 17:31:09 -0500

    Cleaned up the project a bit

--- Assets/Project/Scripts/AttackOnStop.cs
+++ Assets/Project/Scripts/AttackOnStop.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+// Written by Esai Solorio
+// May 22, 2015
 public class AttackOnStop : MonoBehaviour {
 
 	// Use this for initialization
--- Assets/Project/Scripts/ChangeProjectiles.cs
+++ Assets/Project/Scripts/ChangeProjectiles.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+// Written by Esai Solorio
+// May 22, 2015
 public class ChangeProjectiles : MonoBehaviour {
 
 	public FlingProjectilePrediction playerTower;
