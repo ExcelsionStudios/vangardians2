6b0a4b8ec4c237d25dd024ac5a96ee48def9b0b1
Esai Solorio <sanziosan@live.com>
2015-05-23 09:43:33 -0500

    Tiny commit, added a way to quit the application, it will only work on an official build not in the Unity Editor, so build it and run before u try it

---
+++ Assets/QuitScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitScript : MonoBehaviour {
+
+	// Use this for initialization
+		public void quitApplication(){
+		Application.Quit ();
+	}
+}
