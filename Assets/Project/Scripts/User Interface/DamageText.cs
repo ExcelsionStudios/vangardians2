ff12b49cc8c67a538f1f1d05520e86dc1c65234b
Epitome87 <rubywep@hotmail.com>
2015-05-23 22:00:04 -0700

    - Trying to get text to appear over an enemy, which will later only display when he is hit, showing how much damage was done. No luck :(

---
+++ Assets/Project/Scripts/User Interface/DamageText.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public static class CanvasExtensions
+{
+	public static Vector2 WorldToCanvas(this Canvas canvas,
+	                                    Vector3 world_position,
+	                                    Camera camera)
+	{
+		if (camera == null)
+		{
+			camera = Camera.main;
+		}
+		
+		Vector3 viewport_position = camera.WorldToViewportPoint(world_position);
+		RectTransform canvas_rect = canvas.GetComponent<RectTransform>();
+		
+		return new Vector2((viewport_position.x * canvas_rect.sizeDelta.x) - (canvas_rect.sizeDelta.x * 0.5f),
+		                   (viewport_position.y * canvas_rect.sizeDelta.y) - (canvas_rect.sizeDelta.y * 0.5f));
+	}
+}
+
+public class DamageText : MonoBehaviour 
+{
+	public Canvas canvas;
+	public RectTransform healthBar;
+	public Transform objectToFollow;
+
+
+	public Text damageText;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		// _ui_canvas being the Canvas, _world_point being a point in the world
+//		RectTransform rectTrans = healthBar.GetComponent<RectTransform>();
+//		rectTrans.anchoredPosition = canvas.WorldToCanvas(objectToFollow.transform.position, Camera.main);
+	}
+}
