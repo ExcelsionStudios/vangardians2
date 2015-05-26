5841e29bef04c410005b921f0e52e9d10eb02aad
Esai Solorio <sanziosan@live.com>
2015-05-26 12:05:19 -0500

    Made a fireball prefab

---
+++ Assets/Project/Scripts/CloseFollowTarget.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseFollowTarget : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = target.transform.position;
+	}
+}
