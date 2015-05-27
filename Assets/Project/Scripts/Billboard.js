21b1b0de156e9d907d12c5d7caa0d2660cb873f6
Esai Solorio <sanziosan@live.com>
2015-05-27 12:44:07 -0500

    Added Health to enemies

---
+++ Assets/Project/Scripts/Billboard.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var target : Transform;
+function Start () {
+
+}
+
+function Update () {
+	
+	gameObject.transform.rotation.z = target.rotation.z;
+	gameObject.transform.rotation.y = target.rotation.y;
+	
+}
