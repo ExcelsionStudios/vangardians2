2ffe0d5efd4d77591bb84cf50c95fee5ae0eb2fe
Epitome87 <rubywep@hotmail.com>
2015-05-27 11:41:39 -0700

    - Edited Billboard to face camera more accurately. - Made prefabs with Esai's new health bars. We'll remove the old prefabs without them later, once other programmers catch up on the new additions. - Made a Health Bar UI script to actually do something with Esai's sliders. It's sloppy for now -- having to set the maximum value for the Slider to that of the Enemy's Max Health, but we'll tweak that out later.

--- Assets/Project/Scripts/Billboard.js
+++ Assets/Project/Scripts/Billboard.js
@@ -1,12 +1,17 @@
 ﻿#pragma strict
 var target : Transform;
-function Start () {
+
+function Start () 
+{
 
 }
 
-function Update () {
+function Update () 
+{
 	
-	gameObject.transform.rotation.z = target.rotation.z;
-	gameObject.transform.rotation.y = target.rotation.y;
+//	gameObject.transform.rotation.z = target.rotation.z;
+//	gameObject.transform.rotation.y = target.rotation.y;
 	
+	// Matt: The above wasn't working. TODO: Make it LookAt camera of our choice, in case we aren't using main.
+	transform.LookAt(Camera.main.transform);
 }
---
+++ Assets/Project/Scripts/User Interface/EnemyHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Matt McGrath 5/27/2015
+
+// Simple script which should be attached only to Enemy prefabs that contain an Enemy script. This will control the prefab's slider and use it as a health bar.
+public class EnemyHealthBar : MonoBehaviour 
+{
+	public Slider healthSlider;				// Reference to the slider that will act as the health bar.
+
+	private Enemy enemyReference;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// We KNOW (or should) that this script is attached to an Enemy prefab with an Enemy script, so grab a reference to it.
+		enemyReference = this.gameObject.GetComponent<Enemy>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Update the slider to have a value matching the Enemy's health.
+		healthSlider.value = enemyReference.Health;
+	}
+}
