d3c390b837a13085169735523a4813758b6bb670
Epitome87 <rubywep@hotmail.com>
2015-05-27 14:03:04 -0700

    - Desperate last minute changes to have SOMETHING not be broken at the last minute

--- Assets/Project/Scripts/ExplodeOnEnemyImpact.cs
+++ Assets/Project/Scripts/ExplodeOnEnemyImpact.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace UnityStandardAssets.Effects{
-	public class ExplodeOnEnemyImpact : MonoBehaviour {
+namespace UnityStandardAssets.Effects
+{
+	public class ExplodeOnEnemyImpact : MonoBehaviour 
+	{
 		public GameObject explodePrefab;
-	// Use this for initialization
-		void OnCollisionEnter(Collision col){
-			if (col.gameObject.tag == "Enemy") {
+	
+		// Use this for initialization
+		void OnCollisionEnter(Collision col)
+		{
+			if (col.gameObject.tag == "Enemy") 
+			{
 				Destroy (gameObject);
 				GameObject newExplosion = (GameObject)Instantiate (explodePrefab, gameObject.transform.position, explodePrefab.transform.rotation);
 				newExplosion.GetComponent<ParticleSystemMultiplier>().multiplier = .25f;
 				newExplosion.GetComponent<ExplosionPhysicsForce>().explosionForce = 8;
 
-				Destroy (col.gameObject);
+				// Don't Destroy anymore! Enemies now have health. Destroying will occur in the DamageEnemy script.
+				//Destroy(col.gameObject);
 			}
 		}
 	}
