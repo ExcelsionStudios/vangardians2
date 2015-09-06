7f412be7f11dbf340116bf9859fcf2265f5ef063
Epitome87 <rubywep@hotmail.com>
2015-09-06 08:40:56 -0700

    - Organization work. - Cleaning up even more unused files -- we're almost done with all garbage ones! - Tweaked a few things to work with the addition of using Enemies namespace.

--- Assets/Project/Scripts/Enemies/EnemyWave.cs
+++ Assets/Project/Scripts/Enemies/EnemyWave.cs
@@ -7,81 +7,83 @@ using UnityEngine;
 // will spawn, what type of enemies will appear, etc. 
 using System.Collections.Generic;
 
-
-public class EnemyWave : MonoBehaviour 
+namespace Enemies 
 {
-	public List<Enemy> activeEnemies;		// List of Enemy objects that have spawned already (and are still alive).
-	public int TotalNumberOfEnemies;		// Number of total enemies that will spawn this wave.
-	public int NumberOfEnemiesSpawned;		// Number of enemies that have already spawned.
-	public bool IsWaveComplete;				// TODO: Make this based on...something.
-	public float WaveTime;					// How long (in seconds) will wave last? (TODO: Or will the wave last / end based on other conditions? Design info needed)
-	public float waveTimeRemaining;		// How long (in seconds) until the wave is ending (stopping enemies from being spawned).
-	public GameObject[] SpawnPoints;		// An array storing all possible points enemies can spawn from.
-	public float SpawnRate;					// The rate (in seconds) at which a new enemy will spawn. (TODO: Perhaps each spawn point hsa this attribute, maybe make Spawners its own small class).
-	private float spawnRateTimer;			// Timer helper that counts down from SpawnRate.
-	private int previousSpawn;
-
-	public int ScoreThisWave;				// Current Score Player has earned during this wave. Perhaps we'll display and rank him after each wave.
-	public bool IsWaveInProgress;			// Is the wave currently in progress, or are we in between waves (perhaps doing post-wave stuff like Upgrading)
-
-
-
-
-	// Use this for initialization
-	void Start () 
+	public class EnemyWave : MonoBehaviour 
 	{
-		foreach (GameObject spawner in SpawnPoints)
+		public List<Enemy> activeEnemies;		// List of Enemy objects that have spawned already (and are still alive).
+		public int TotalNumberOfEnemies;		// Number of total enemies that will spawn this wave.
+		public int NumberOfEnemiesSpawned;		// Number of enemies that have already spawned.
+		public bool IsWaveComplete;				// TODO: Make this based on...something.
+		public float WaveTime;					// How long (in seconds) will wave last? (TODO: Or will the wave last / end based on other conditions? Design info needed)
+		public float waveTimeRemaining;		// How long (in seconds) until the wave is ending (stopping enemies from being spawned).
+		public GameObject[] SpawnPoints;		// An array storing all possible points enemies can spawn from.
+		public float SpawnRate;					// The rate (in seconds) at which a new enemy will spawn. (TODO: Perhaps each spawn point hsa this attribute, maybe make Spawners its own small class).
+		private float spawnRateTimer;			// Timer helper that counts down from SpawnRate.
+		private int previousSpawn;
+
+		public int ScoreThisWave;				// Current Score Player has earned during this wave. Perhaps we'll display and rank him after each wave.
+		public bool IsWaveInProgress;			// Is the wave currently in progress, or are we in between waves (perhaps doing post-wave stuff like Upgrading)
+
+
+
+
+		// Use this for initialization
+		void Start () 
 		{
-			spawner.SetActive(true);
+			foreach (GameObject spawner in SpawnPoints)
+			{
+				spawner.SetActive(true);
+			}
+
+			IsWaveInProgress = true;
+			IsWaveComplete = false;
+
+			waveTimeRemaining = WaveTime;
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			if (!IsWaveInProgress)
+				return;
+
+			//spawnRateTimer += 1.0f * Time.deltaTime;
+			waveTimeRemaining -= 1.0f * Time.deltaTime;
+
+	//		if (spawnRateTimer >= SpawnRate)
+	//		{
+	//			spawnRateTimer = 0.0f;
+	//			SpawnEnemy();
+	//		}
+
+			if (waveTimeRemaining <= 0f)
+			{
+				EndWave();
+			}
+
 		}
 
-		IsWaveInProgress = true;
-		IsWaveComplete = false;
-
-		waveTimeRemaining = WaveTime;
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		if (!IsWaveInProgress)
-			return;
-
-		//spawnRateTimer += 1.0f * Time.deltaTime;
-		waveTimeRemaining -= 1.0f * Time.deltaTime;
-
-//		if (spawnRateTimer >= SpawnRate)
-//		{
-//			spawnRateTimer = 0.0f;
-//			SpawnEnemy();
-//		}
-
-		if (waveTimeRemaining <= 0f)
+		// TODO: Change EnemySpawner or utilize it here.
+		void SpawnEnemy()
 		{
-			EndWave();
+
 		}
 
-	}
+		public void EndWave()
+		{
+			Debug.Log("Ending Current Wave");
+			IsWaveInProgress = false;
+			IsWaveComplete = true;
+	//		this.gameObject.SetActive(false);
+	//		Destroy(this.gameObject, 0.5f);
+		}
 
-	// TODO: Change EnemySpawner or utilize it here.
-	void SpawnEnemy()
-	{
-
-	}
-
-	public void EndWave()
-	{
-		Debug.Log("Ending Current Wave");
-		IsWaveInProgress = false;
-		IsWaveComplete = true;
-//		this.gameObject.SetActive(false);
-//		Destroy(this.gameObject, 0.5f);
-	}
-
-	public void BeginWave()
-	{
-		Debug.Log("Starting New Wave");
-		IsWaveInProgress = true;
-		IsWaveComplete = false;
+		public void BeginWave()
+		{
+			Debug.Log("Starting New Wave");
+			IsWaveInProgress = true;
+			IsWaveComplete = false;
+		}
 	}
 }
--- Assets/Project/Scripts/Enemies/EnemyWaveManager.cs
+++ Assets/Project/Scripts/Enemies/EnemyWaveManager.cs
@@ -4,102 +4,105 @@ using UnityEngine;
 
 // Matt McGrath, 5/23/2015
 
-// The Wave Manager manages how Waves of Enemies are handled. Here we can determine what events / enemies appear during which Waves.
-public class EnemyWaveManager : MonoBehaviour 
+namespace Enemies 
 {
-	// A list of Waves the Manager is controlling.
-	public List<EnemyWave> waves;
-
-	// The number of the current wave.
-	public int CurrentWaveNumber;
-
-
-	// BEING LAZYYYYY: For Prototype, let's Prefab "Waves" and drag them in here.  Can be a list or array later.
-	public List<GameObject> prefabedWaves;
-	public EnemyWave currentPrefabedWave;
-
-
-	private float gracePeriodBetweenWaves = 5.0f;
-	private float gracePeriodTimer;
-
-	// Use this for initialization
-	void Start () 
+	// The Wave Manager manages how Waves of Enemies are handled. Here we can determine what events / enemies appear during which Waves.
+	public class EnemyWaveManager : MonoBehaviour 
 	{
-		// We start on Wave 1.
-		CurrentWaveNumber = 1;
+		// A list of Waves the Manager is controlling.
+		public List<EnemyWave> waves;
 
-		SetUpPrefabedWave(CurrentWaveNumber);
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		//if (waves[CurrentWaveNumber - 1].IsWaveComplete)
+		// The number of the current wave.
+		public int CurrentWaveNumber;
+
+
+		// BEING LAZYYYYY: For Prototype, let's Prefab "Waves" and drag them in here.  Can be a list or array later.
+		public List<GameObject> prefabedWaves;
+		public EnemyWave currentPrefabedWave;
+
+
+		private float gracePeriodBetweenWaves = 5.0f;
+		private float gracePeriodTimer;
+
+		// Use this for initialization
+		void Start () 
 		{
-			// Some pausing? Some menu? 
+			// We start on Wave 1.
+			CurrentWaveNumber = 1;
 
-			// Give the player a chance to spend his Money on upgrades?
-
-			// Cleanup the previous wave, in case of memory leaks.
-
-			//Then go onto the next wave.
+			SetUpPrefabedWave(CurrentWaveNumber);
 		}
-
-		if (currentPrefabedWave != null && currentPrefabedWave.IsWaveComplete)
+		
+		// Update is called once per frame
+		void Update () 
 		{
-			Debug.Log ("Wave Complete! New One Starts In " + gracePeriodTimer);
-			gracePeriodTimer -= Time.deltaTime;
-			if (gracePeriodTimer <= 0f)
+			//if (waves[CurrentWaveNumber - 1].IsWaveComplete)
 			{
-				currentPrefabedWave.gameObject.SetActive(false);
-				Destroy(currentPrefabedWave);
+				// Some pausing? Some menu? 
 
-				SetUpPrefabedWave(++CurrentWaveNumber);
+				// Give the player a chance to spend his Money on upgrades?
+
+				// Cleanup the previous wave, in case of memory leaks.
+
+				//Then go onto the next wave.
+			}
+
+			if (currentPrefabedWave != null && currentPrefabedWave.IsWaveComplete)
+			{
+				Debug.Log ("Wave Complete! New One Starts In " + gracePeriodTimer);
+				gracePeriodTimer -= Time.deltaTime;
+				if (gracePeriodTimer <= 0f)
+				{
+					currentPrefabedWave.gameObject.SetActive(false);
+					Destroy(currentPrefabedWave);
+
+					SetUpPrefabedWave(++CurrentWaveNumber);
+				}
 			}
 		}
-	}
 
-	void SetUpPrefabedWave(int waveNumber)
-	{
-//
-//		if (waveNumber >= prefabedWaves.Count);
-//		    return;
-
-		Debug.Log("Setting Up Next Wave");
-		GameObject go = prefabedWaves[waveNumber - 1];
-		currentPrefabedWave = go.GetComponent<EnemyWave>();
-		currentPrefabedWave.gameObject.SetActive(true);
-	
-		gracePeriodTimer = gracePeriodBetweenWaves;
-
-		currentPrefabedWave.BeginWave();
-	}
-
-	// Sets up the Enemy Wave number specified in the parameter. For now let's use a switch-case to keep things simple.
-	void SetUpWave(int waveNumber)
-	{
-		EnemyWave wave = new EnemyWave();
-
-		switch (waveNumber)
+		void SetUpPrefabedWave(int waveNumber)
 		{
-		case 1:
-			wave.TotalNumberOfEnemies = 5;
-			wave.WaveTime = 30f;
-			break;
-		case 2:
-			wave.TotalNumberOfEnemies = 10;
-			wave.WaveTime = 60f;
-			break;
-		case 3:
-			wave.TotalNumberOfEnemies = 15;
-			wave.WaveTime = 90f;
-			break;
-		default:
-			wave.TotalNumberOfEnemies = 10;
-			wave.WaveTime = 60f;
-			break;
+	//
+	//		if (waveNumber >= prefabedWaves.Count);
+	//		    return;
+
+			Debug.Log("Setting Up Next Wave");
+			GameObject go = prefabedWaves[waveNumber - 1];
+			currentPrefabedWave = go.GetComponent<EnemyWave>();
+			currentPrefabedWave.gameObject.SetActive(true);
+		
+			gracePeriodTimer = gracePeriodBetweenWaves;
+
+			currentPrefabedWave.BeginWave();
 		}
 
-		waves.Add(wave);
+		// Sets up the Enemy Wave number specified in the parameter. For now let's use a switch-case to keep things simple.
+		void SetUpWave(int waveNumber)
+		{
+			EnemyWave wave = new EnemyWave();
+
+			switch (waveNumber)
+			{
+			case 1:
+				wave.TotalNumberOfEnemies = 5;
+				wave.WaveTime = 30f;
+				break;
+			case 2:
+				wave.TotalNumberOfEnemies = 10;
+				wave.WaveTime = 60f;
+				break;
+			case 3:
+				wave.TotalNumberOfEnemies = 15;
+				wave.WaveTime = 90f;
+				break;
+			default:
+				wave.TotalNumberOfEnemies = 10;
+				wave.WaveTime = 60f;
+				break;
+			}
+
+			waves.Add(wave);
+		}
 	}
 }
